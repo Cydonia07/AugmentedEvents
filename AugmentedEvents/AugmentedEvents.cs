27595209e109614d19927fd61e7e13d9412f63eb
Mike Kloszewski <mikloszewski@gmail.com>
2018-06-16 12:22:43 -0500

    Reading settings from external file, added Mod and Settings json to repo

--- AugmentedEvents/AugmentedEvents.cs
+++ AugmentedEvents/AugmentedEvents.cs
@@ -6,10 +6,10 @@ using BattleTech;
 using Harmony;
 using HBS.Logging;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace AugmentedEvents
 {
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
     [HarmonyPatch(typeof(SimGameState), "OnEventOptionSelected")]
     public static class SimGameState_OnEventOptionSelected_Patch
     {
@@ -26,13 +26,13 @@ namespace AugmentedEvents
                             switch (eventResult.Stats[i].name)
                             {
                                 case "Morale":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * ModSettings.MoraleMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * AugmentedEvents.Settings.MoraleMultiplier).ToString();
                                     break;
                                 case "MechTechSkill":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * ModSettings.MechTechSkillMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * AugmentedEvents.Settings.MechTechSkillMultiplier).ToString();
                                     break;
                                 case "MedTechSkill":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * ModSettings.MedTechSkillMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) * AugmentedEvents.Settings.MedTechSkillMultiplier).ToString();
                                     break;
                             }
                         }
@@ -43,7 +43,6 @@ namespace AugmentedEvents
         }
     }
 
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
     [HarmonyPatch(typeof(SimGameState), "OnEventDismissed")]
     public static class SimGameState_OnEventDismissed_Patch
     {
@@ -60,13 +59,13 @@ namespace AugmentedEvents
                             switch (eventResult.Stats[i].name)
                             {
                                 case "Morale":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / ModSettings.MoraleMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / AugmentedEvents.Settings.MoraleMultiplier).ToString();
                                     break;
                                 case "MechTechSkill":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / ModSettings.MechTechSkillMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / AugmentedEvents.Settings.MechTechSkillMultiplier).ToString();
                                     break;
                                 case "MedTechSkill":
-                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / ModSettings.MedTechSkillMultiplier).ToString();
+                                    eventResult.Stats[i].value = (int.Parse(eventResult.Stats[i].value) / AugmentedEvents.Settings.MedTechSkillMultiplier).ToString();
                                     break;
                             }
                         }
@@ -76,22 +75,36 @@ namespace AugmentedEvents
         }
     }
 
-    internal static class ModSettings
+    internal class ModSettings
     {
-        public static int MoraleMultiplier = 5;
-        public static int MechTechSkillMultiplier = 2;
-        public static int MedTechSkillMultiplier = 2;
+        public int MoraleMultiplier = 5;
+        public int MechTechSkillMultiplier = 2;
+        public int MedTechSkillMultiplier = 2;
     }
 
     public static class AugmentedEvents
     {
         internal static ILog Logger = HBS.Logging.Logger.GetLogger("AugmentedEvents");
+        internal static ModSettings Settings;
         internal static SimGameEventOption LastModifiedOption;
 
-        public static void Init()
+        public static void Init(string modDir, string modSettings)
         {
             var harmony = HarmonyInstance.Create("io.github.cydonia07.AugmentedEvents");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("mods/AugmentedEvents/Settings.json"))
+                {
+                    string settingsJson = reader.ReadToEnd();
+                    Settings = JsonConvert.DeserializeObject<ModSettings>(settingsJson);
+                }
+            }
+            catch (Exception)
+            {
+                Settings = new ModSettings();
+            }
         }
     }
 }
